b103349efe4bf53a50b16cb173be848b250dd89c
Joao Pela <joao@rotageek.com>
2017-02-20 15:31:20 +0000

    Starting a new FSharp based project to follow the book "Mastering .NET Machine Learning"

---
+++ FSharp/Book_Master_DotNet_ML/FirstFSharpProject/FirstFSharpProject/FirstProgram.fs
@@ -0,0 +1,23 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open System
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+
+    // Printing a line to the console
+    let x = "Hello World"
+    Console.WriteLine(x)
+
+    // SECTION: Handling arrays 
+    let ints = [|1;2;3;4;5;6;|] // Creating
+
+    // We can "pipe" the arrays into other functions
+    let summedValue = ints |> Array.sum
+    let multiplied = ints |> Array.map (fun i -> i * 2)
+
+    
+    let y = Console.ReadKey() // Reading a line from the console
+    0 // return an integer exit code
+
---
+++ FSharp/Book_Master_DotNet_ML/FirstFSharpProject/FirstFSharpProject/HelloWorldScript.fsx
@@ -0,0 +1,11 @@
+﻿open System
+
+let x = "Hello World"
+
+let ints = [|1;2;3;4;5;6;|]
+
+printfn "%A" ints
+
+let summedValue = ints |> Array.sum
+
+let multiplied = ints |> Array.map (fun i -> i * 2)
